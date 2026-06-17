c5d20500fab748a62e878b6b8504cc1d29ca26c5
AyushSinha1214 <ayushsinha1214@gmail.com>
2026-06-17 16:50:00 +0530

    initial backend setup done

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -12,6 +12,36 @@ app.get("/", (req, res) => {
     res.json({
         message: "RepoLens Backend Running 🚀"
     });
+});
+
+// GitHub Repo Route
+app.get("/api/repo/:owner/:repo", async (req, res) => {
+    try {
+        const { owner, repo } = req.params;
+
+        const response = await axios.get(
+            `https://api.github.com/repos/${owner}/${repo}`
+        );
+
+        const repoData = {
+    name: response.data.name,
+    description: response.data.description,
+    stars: response.data.stargazers_count,
+    forks: response.data.forks_count,
+    issues: response.data.open_issues_count,
+    language: response.data.language,
+    createdAt: response.data.created_at,
+    updatedAt: response.data.updated_at,
+    url: response.data.html_url
+};
+
+res.json(repoData);
+
+    } catch (error) {
+        res.status(404).json({
+            message: "Repository not found"
+        });
+    }
 });
 
 const PORT = 5000;
