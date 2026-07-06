6d0bc257d98b7e3bca63d5dcbb23557e8979a07a
AyushSinha1214 <ayushsinha1214@gmail.com>
2026-07-06 18:47:55 +0530

    deploying touch

---
+++ server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const repoRoutes = require("./routes/repoRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log("Mongo Error:", err.message));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "RepoLens Backend Running 🚀",
+  });
+});
+
+app.use("/api", repoRoutes);
+app.use("/api/auth", authRoutes);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
